cd06f0848569e94ef09e0f2c4cc0591f7adadf48
Avoaja Joshua Akachi <akachijc@gmail.com>
2022-04-02 19:31:45 +0100

    Update Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -48,7 +48,7 @@ function Layout() {
         <div className="flex fixed bottom-0 h-[47px] z-50 md:h-auto bg-black right-0 left-0 md:relative flex-row items-center md:items-start md:flex-col overflow-x-auto divide-y divide-x md:divide-x-0 md:pt-6 divide-slate-400">
           <CustomLink
             to="/"
-            className="text-slate-300 w-1/3 md:w-full hover:text-portfolio-skyblue hover:font-bold transition-all duration-1000 ease-in-out border-t border-slate-400 flex flex-shrink-0 items-center px-3 h-[inherit] md:py-4 justify-center"
+            className="text-slate-300 w-1/2 md:w-full hover:text-portfolio-skyblue hover:font-bold transition-all duration-1000 ease-in-out border-t border-slate-400 flex flex-shrink-0 items-center px-3 h-[inherit] md:py-4 justify-center"
           >
             Home
           </CustomLink>
@@ -60,7 +60,7 @@ function Layout() {
           </CustomLink> */}
           <CustomLink
             to={user ? "/rooms/" + user.get("ethAddress") : "/rooms/create"}
-            className="text-slate-300 w-1/3 md:w-full h-[inherit] flex items-center justify-center py-2 md:py-4 hover:text-portfolio-skyblue hover:font-bold transition-all duration-1000 ease-in-out flex-shrink-0 px-3"
+            className="text-slate-300 w-1/2 md:w-full h-[inherit] flex items-center justify-center py-2 md:py-4 hover:text-portfolio-skyblue hover:font-bold transition-all duration-1000 ease-in-out flex-shrink-0 px-3"
           >
             {user ? "My Web3Gram" : "Create My Web3Gram"}
           </CustomLink>
